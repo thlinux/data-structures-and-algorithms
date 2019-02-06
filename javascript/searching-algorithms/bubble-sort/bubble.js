1a72ed72089ebbede536b399821d9b3d6ba26bf8
Tristan Hull <soldier@kenpokicks.com>
2019-02-05 23:33:40 -0800

    bubble sort optimized

--- javascript/searching-algorithms/bubble-sort/bubble.js
+++ javascript/searching-algorithms/bubble-sort/bubble.js
@@ -6,14 +6,19 @@
 
 function bubbleSort(arr) {
     const swap = (arr, idx1, idx2) => { [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]] }
+    let noSwaps;
     for(let i = arr.length; i > 0; i--) {
+        noSwaps = true;
         for(let j = 0; j < i-1; j++) {
+            console.log(arr, arr[j], arr[j+1])
             if(arr[j] > arr[j + 1]) {
                 swap(arr, j, j+1)
+                noSwaps = false;
             }
         }
+        if(noSwaps) break
     }
     return arr;
 }
 
-console.log(bubbleSort([37,45,29,8]))
+console.log(bubbleSort([8,1,2,3,4,5,6,7]))

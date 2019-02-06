d0f43d76996908caf9d4105a3b019f7e1b291900
Tristan Hull <soldier@kenpokicks.com>
2019-02-05 18:40:53 -0800

    binary search

---
+++ javascript/searching-algorithms/binary/binary.js
@@ -0,0 +1,14 @@
+function binarySearch(arr, val) {
+    let start = 0;
+    let end = arr.length - 1;
+    let middle = Math.floor((start + end) / 2);
+    
+    while(arr[middle] !== val && start <= end) {
+        if(val < arr[middle]) end = middle - 1; 
+        else start = middle + 1;
+        middle = Math.floor((start + end) / 2);
+    }
+    return arr[middle] === val ? middle : -1;
+}
+
+console.log(binarySearch([2,5,6,7,8,9], 9))

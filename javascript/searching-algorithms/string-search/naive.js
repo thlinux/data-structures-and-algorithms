e390c27b652de4faf21a1a54ea60c03143b102e1
Tristan Hull <soldier@kenpokicks.com>
2019-02-05 19:11:08 -0800

    naive search

--- javascript/searching-algorithms/string-search/naive.js
+++ javascript/searching-algorithms/string-search/naive.js
@@ -5,3 +5,16 @@
 //if chars DO match, keep going
 //if you complete inner loop and find a match, ++ the counter
 //return counter
+
+function naiveSearch(long, short) {
+    let count = 0;
+    for(let i=0; i<long.length; i++){
+        for(let j=0; j<short.length; j++){
+            if(short[j] !== long[i + j]) break
+            if(j === short.length - 1) count++
+        }
+    }
+    return count;
+}
+
+console.log(naiveSearch('lorie loled', 'lo'))

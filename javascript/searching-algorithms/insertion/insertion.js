73f75e108a8e2ed40b7d5db40603581840dfbd23
Tristan Hull <soldier@kenpokicks.com>
2019-02-06 00:04:35 -0800

    insertion sort

---
+++ javascript/searching-algorithms/insertion/insertion.js
@@ -0,0 +1,12 @@
+//O()
+//start by picking second element of array
+//compare the second element with the one before it and swap if needed
+//continue to the next element and if it is in the incorrect order, iterate through the sorted portion to place the element in the correct place
+//repeat until sorted
+
+function insertonSort(arr) {
+    
+    return arr;
+}
+
+console.log(insertonSort([2,1,9,76,4]))

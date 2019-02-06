8345b951ba5873690700d6f029b6505373d924de
Tristan Hull <soldier@kenpokicks.com>
2019-02-05 16:27:42 -0800

    readablility to linear search

--- javascript/searching-algorithms/linear.js
+++ javascript/searching-algorithms/linear.js
@@ -1,8 +1,11 @@
+const array = ['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
+const value = 'Delaware'
+
 function linearSearch(arr, val) {
     for(let i = 0; i < arr.length; i++) {
-        if(arr[i] === val) {
-            return i;
-        }
+        if(arr[i] === val) return i;
     }
     return -1;
 }
+
+console.log(value + " located at: " + linearSearch(array, value))
